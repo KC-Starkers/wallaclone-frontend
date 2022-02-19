7cc426e7ec7cd0a83a397fb8ce200e264f985964
Ivan Martinez <ivmartinezc@gmail.com>
2022-02-19 11:21:22 +0100

    vuelta a empezar, nuevo repo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,9 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           <code>src/App.js</code> and save to reload.
+          <code>src/App.js</code> and save to reload.
         </p>
-       <h2>Starkers</h2> 
+
         <a
           className="App-link"
           href="https://reactjs.org"
