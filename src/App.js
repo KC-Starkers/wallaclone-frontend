d41099c7cf65fda4dbf59b4156334c172c8b5ef9
Ivan Martinez <ivmartinezc@gmail.com>
2022-02-19 11:09:06 +0100

    agrego h1 Starkers en App.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           <code>src/App.js</code> and save to reload.
         </p>
+       <h2>Starkers</h2> 
         <a
           className="App-link"
           href="https://reactjs.org"
