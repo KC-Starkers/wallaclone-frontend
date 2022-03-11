253d463339b17fb5c958d98b7a2540e27d49ce3d
BEATRIZ MARTÍN GARRIDO <martin_beatriz94@hotmail.com>
2022-03-11 16:55:21 +0100

    WIP:BMG:barra navegacion

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React, { useState } from 'react';
 import Header from './components/layout/header';
@@ -8,34 +8,7 @@ function App() {
   
   return (
     <Header />
-    /*
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>src/App.js</code> and save to reload.
-        </p>
-               
-       <div class="flex items-center justify-center ">
-    <div class="flex border-2 border-gray-200 rounded">
-        <input type="text" class="px-4 py-2 w-80 text-slate-900" placeholder="Search..."></input>
-        <button class="px-4 text-white bg-gray-600 border-l ">
-            Search
-        </button>
-    </div>
-</div>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-    */
+  
   );
 }
 
--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+
 import React, { useState } from 'react';
-import Header from './components/layout/header';
+import Header from './layout/header';
 
 
 function App() {
   
   return (
-    <Header />
-    /*
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>src/App.js</code> and save to reload.
-        </p>
-               
-       <div class="flex items-center justify-center ">
-    <div class="flex border-2 border-gray-200 rounded">
-        <input type="text" class="px-4 py-2 w-80 text-slate-900" placeholder="Search..."></input>
-        <button class="px-4 text-white bg-gray-600 border-l ">
-            Search
-        </button>
-    </div>
-</div>
-
-        <h2>Starkers Team</h2>
-        <h4>Push Alex</h4>
-        <h4>Push Ivan M.</h4>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-    */
+   <Header />
   );
 }
 
