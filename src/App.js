a4dda6b26b05a901a267f274896050cd66703a3f
BEATRIZ MARTÍN GARRIDO <martin_beatriz94@hotmail.com>
2022-03-11 16:28:16 +0100

    WIP:BMG:mergeredux

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react';
+import Header from './components/layout/header';
+
+
+function App() {
+  
+  return (
+    <Header />
+    /*
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>src/App.js</code> and save to reload.
+        </p>
+               
+       <div class="flex items-center justify-center ">
+    <div class="flex border-2 border-gray-200 rounded">
+        <input type="text" class="px-4 py-2 w-80 text-slate-900" placeholder="Search..."></input>
+        <button class="px-4 text-white bg-gray-600 border-l ">
+            Search
+        </button>
+    </div>
+</div>
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+    */
+  );
+}
+
+export default App;
