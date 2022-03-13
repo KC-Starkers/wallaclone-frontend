48e6461042072f1fee7b4c25702fd624454cb59a
Luis <40238711+mogurimogu@users.noreply.github.com>
2022-03-13 19:28:47 +0100

    Marcadas tareas por realizar en este punto y añadida condición para reutilizar este conponente para el resto de usuarios

--- src/components/profile/myServices/MyServices.js
+++ src/components/profile/myServices/MyServices.js
@@ -5,10 +5,17 @@ import { Link } from "react-router-dom";
 const MyServices = (userId) => {
   const [services, setServices] = useState([]);
 
-
   //Busca y filtra los anuncios por ID
   useEffect(() => {
     async function getServiceDetail() {
+      //TODO recoger el usuario y devolverlo
+      const getUserId = () => {
+        if (!userId) {
+          return; //TODO recoger el id del usuario logueado
+        } else {
+          return userId;
+        }
+      };
       //Conexión con la api
       const connection = axios.create({
         baseURL: `${process.env.REACT_APP_API_BASE_URL}/services/`,
@@ -16,7 +23,7 @@ const MyServices = (userId) => {
       try {
         const allServices = await connection.get();
         const selectedServices = (adverts) => {
-          return adverts.createdBy.userId === 1;
+          return adverts.createdBy.userId === getUserId;
         };
         const filteredServices = allServices.data.filter(selectedServices);
         setServices(filteredServices);
@@ -38,7 +45,7 @@ const MyServices = (userId) => {
                 <Link to={`/service/${service.id}`}>
                   <article>
                     <img src={service.advertImage} alt={service.name} />
-                    {/* Cambiar por el elemento offertype badge */}
+                    {/*TODO Cambiar por el elemento offertype badge */}
                     <div>{service.offerAdvert ? "Buscan" : "Ofrecen"}</div>
                     <div>{service.price}</div>
                     <h1>{service.name}</h1>
