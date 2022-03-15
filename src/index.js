6a491db8b1e8a1530a40738f2962203af935191d
Ivan Martinez <ivmartinezc@gmail.com>
2022-03-15 18:55:07 +0100

    elimino codigo de la plantilla de create-react-app

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
-import Router from "./elements/Router";
+import Router from "./Router";
 import { Provider } from "react-redux";
-
-
-//TODO: eliminar Root.js y App.js (que en wallaclone es Router.js)
-
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
+ {/*    <Provider store={store}> */}
       <Router />
-    </Provider>
+  {/*   </Provider> */}
   </React.StrictMode>,
   document.getElementById("root")
 );
