88120a73f4fdbb010b5ff0354c0571e09edd0fde
Ivan Martinez <ivmartinezc@gmail.com>
2022-03-15 17:25:57 +0100

    END:IMC: Envuelvo Router en el Provider de react-redux

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import Router from "./components/Router";
+import Router from "./elements/Router";
+import { Provider } from "react-redux";
+
+
+//TODO: eliminar Root.js y App.js (que en wallaclone es Router.js)
+
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router/>
+    <Provider store={store}>
+      <Router />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
