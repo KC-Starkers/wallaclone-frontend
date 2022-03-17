bc311a17b321915b6434e62d190220f37c8b8c37
Ivan Martinez <ivmartinezc@gmail.com>
2022-03-17 21:10:20 +0100

    WIP:IMC: componente signup: llamada al api

---
+++ src/components/auth/SignUpPage/apicalls.js
@@ -0,0 +1,12 @@
+import client from "../../client";
+
+const path = "/auth";
+
+export const signUp = (userData) => {
+  return client
+    .post(path, userData)
+    .then((response) => response.result)
+    .cath((error) => console.log(error)); //TODO: hacer dispatch de este error
+};
+
+
