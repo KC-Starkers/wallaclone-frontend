6bbaa02146b20c773034a47d7fb7a0e500211651
Luis <40238711+mogurimogu@users.noreply.github.com>
2022-03-30 23:17:55 +0200

    subida logos

--- src/components/common/full_logo.js
+++ src/components/common/full_logo.js
@@ -1,7 +1,9 @@
-function FullLogo(){
-    return(
-        <p className="font-sans">Wallaclone</p>
-    )
+import logo from '../../../public/logo-starker.png'
+console.log(logo)
+function FullLogo() {
+  return (
+    <img src={`/logo-starker.png`} alt="starkers" />
+  );
 }
 
-export default FullLogo
+export default FullLogo;
